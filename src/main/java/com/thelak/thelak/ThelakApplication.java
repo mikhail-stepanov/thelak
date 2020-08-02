b6e49fec0756157de3546b85df8225cc67360ec9
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-08-02 23:18:50 +0300

    Initial commit

---
+++ src/main/java/com/thelak/thelak/ThelakApplication.java
@@ -0,0 +1,13 @@
+package com.thelak.thelak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThelakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thelak/thelak/ThelakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thelak.thelak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThelakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
