2f972d56a39c8144ac49737d8aa2e70e7ad37f04
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-08-16 02:52:34 +0500

    swagger fix

--- ms-auth/src/main/java/com/thelak/auth/endpoints/AuthenticationEndpoint.java
+++ ms-auth/src/main/java/com/thelak/auth/endpoints/AuthenticationEndpoint.java
@@ -6,10 +6,15 @@ import com.thelak.route.auth.models.AuthLoginRequest;
 import com.thelak.route.auth.models.AuthSignupRequest;
 import com.thelak.route.auth.models.UserModel;
 import com.thelak.route.exceptions.MicroServiceException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value = "Authorization API", produces = "application/json")
 public class AuthenticationEndpoint implements IAuthenticationService {
 
     @Autowired
@@ -17,6 +22,13 @@ public class AuthenticationEndpoint implements IAuthenticationService {
 
     @Override
     @CrossOrigin
+    @ApiOperation(value = "Get user info by token")
+    @ApiImplicitParams(
+            {@ApiImplicitParam(required = true,
+                    defaultValue = "Bearer ",
+                    name = "Authorization",
+                    paramType = "header")}
+    )
     @RequestMapping(value = AUTH_INFO, method = {RequestMethod.GET})
     public UserModel info() throws MicroServiceException {
         return authenticationService.info();
@@ -24,6 +36,7 @@ public class AuthenticationEndpoint implements IAuthenticationService {
 
     @Override
     @CrossOrigin
+    @ApiOperation(value = "Sign up")
     @RequestMapping(value = AUTH_SIGN_UP, method = {RequestMethod.POST})
     public UserModel signUp(@RequestBody AuthSignupRequest request) throws MicroServiceException {
         return authenticationService.signUp(request);
@@ -31,6 +44,7 @@ public class AuthenticationEndpoint implements IAuthenticationService {
 
     @Override
     @CrossOrigin
+    @ApiOperation(value = "Login user")
     @RequestMapping(value = AUTH_LOGIN, method = {RequestMethod.POST})
     public String login(@RequestBody AuthLoginRequest request) throws MicroServiceException {
         return authenticationService.login(request);
