f48b050259544565064eb331f9b7275e347de67e
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-08-19 01:42:51 +0500

    ms-site

--- ms-site/src/main/java/com/thelak/site/configurations/RestConfiguration.java
+++ ms-site/src/main/java/com/thelak/site/configurations/RestConfiguration.java
@@ -25,6 +25,12 @@ public class RestConfiguration implements WebMvcConfigurer {
         }
     }
 
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.html");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE");
