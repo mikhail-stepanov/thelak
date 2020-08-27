cb4e5a9da32edfd3a0ad93ca61d7934639b1e76d
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-08-27 23:27:51 +0300

    hz

---
+++ ms-database/src/main/java/com/thelak/database/entity/DbVideoRating.java
@@ -0,0 +1,9 @@
+package com.thelak.database.entity;
+
+import com.thelak.database.entity.auto._DbVideoRating;
+
+public class DbVideoRating extends _DbVideoRating {
+
+    private static final long serialVersionUID = 1L; 
+
+}
