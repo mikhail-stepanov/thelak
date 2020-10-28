3d490848d3f49754b679bab0ae1bd2c1f93715c5
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-10-28 22:45:11 +0300

    cert buy

--- ms-payments/src/main/java/com/thelak/payments/endpoints/PaymentEndpoint.java
+++ ms-payments/src/main/java/com/thelak/payments/endpoints/PaymentEndpoint.java
@@ -309,7 +309,7 @@ public class PaymentEndpoint extends AbstractMicroservice implements IPaymentSer
                     dbPaymentsRecurrent.setEmail(reccurentPayRequest.getEmail());
                     dbPaymentsRecurrent.setInterval(reccurentPayRequest.getInterval());
                     dbPaymentsRecurrent.setPeriod(reccurentPayRequest.getPeriod());
-                    dbPaymentsRecurrent.setStartDate(reccurentPayRequest.getStartDate().toLocalDateTime());
+                    dbPaymentsRecurrent.setStartDate(reccurentPayRequest.getStartDate().toLocalDateTime().plusMonths(subscription.getMonths()));
                     dbPaymentsRecurrent.setRequireConfirmation(reccurentPayRequest.getRequireConfirmation());
                     dbPaymentsRecurrent.setToken(reccurentPayRequest.getToken());
                     dbPaymentsRecurrent.setStatus(false);
